4207bf5415735fb08a3148b47faf10a994ae8e22
jeromedecoster <jeromedecoster@users.noreply.github.com>
2020-09-09 10:54:10 +0200

    :boom:

--- site/server.js
+++ site/server.js
@@ -1,7 +1,7 @@
 const app = require('./app.js')
 
 if (process.env.WEBSITE_PORT == null || process.env.WEBSITE_PORT.length == 0) { 
-    throw new Error('WEBSITE__PORT environment variable is required')
+    throw new Error('WEBSITE_PORT environment variable is required')
 }
 
 app.listen(process.env.WEBSITE_PORT, () => { 
